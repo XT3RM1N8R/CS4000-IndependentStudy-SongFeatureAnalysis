8984169f52f9783386e496076dfc8a2d7b22ce24
Hao Nguyen <danielnguyen0403@gmail.com>
2019-04-06 16:36:37 -0500

    added year slider

--- Project2_Data visualization/parallel.js
+++ Project2_Data visualization/parallel.js
@@ -6,7 +6,11 @@ var width = 900, height = 600,
     contentWidth = width - margin.left - margin.right,
     contentHeight = height - margin.top - margin.bottom;
 
-var svg = d3.select("body").append("svg").attr("width",width).attr("height",height),
+//Time Format and Parsing
+const parseTime = d3.timeParse("%m/%d/%Y %H:%M");
+const formatYear = d3.timeFormat("%Y");
+
+var svg = d3.select("#chart-area").append("svg").attr("width",width).attr("height",height),
     g = svg.append("g").attr("transform","translate("+margin.left+","+margin.top+")"),
     titleGroup = svg.append("g").attr("transform","translate("+(margin.left+20)+","+(margin.top-10)+")");
 
@@ -32,13 +36,91 @@ var dragging = {};
 
 let features = [];
 // console.log('44444444');
-d3.csv("dataset/dataset.csv",function (error, songs) {
+d3.csv("dataset/dataset_full(optimal).csv",function (error, songs) {
     // console.log('222222222');
 
     // get features that used for mutlti-dimension coordinates
     features = songs.columns.slice(1, 9);
     xScale.domain(features);
 
+
+    // Doing Time Slider
+    // Time
+
+    songs.forEach(d=>{
+        d.tracks_track_date_created = formatYear(parseTime(d.tracks_track_date_created));
+    });
+    var timeRange = d3.extent(songs,d=>{return d.tracks_track_date_created});
+    var dataTime = d3.range(Number(timeRange[0]),Number(timeRange[1])+1).map(d=>{
+        // console.log(d);
+        return d;
+    });
+
+    //Slider
+    // var slidersvg = d3.select('div#slider').append('svg')
+    //     .attr('width', 100)
+    //     .attr('height', 50);
+    //
+    // var slider = slidersvg.append('g')
+    //     .classed('slider', true);
+    //
+    // // using clamp here to avoid slider exceeding the range limits
+    // var xScale = d3.scaleLinear()
+    //     .domain(range)
+    //     .range([0, width - margin.left - margin.right])
+    //     .clamp(true);
+    //
+    // // array useful for step sliders
+    // var rangeValues = d3.range(range[0], range[1], step || 1).concat(range[1]);
+    // var xAxis = d3.axisBottom(xScale).tickValues(rangeValues).tickFormat(function (d) {
+    //     return d;
+    // });
+    //
+    // xScale.clamp(true);
+
+
+    var sliderTime = d3.sliderBottom()
+        .min(timeRange[0])
+        .max(timeRange[1])
+        .step(1)
+        .width(300)
+        .tickFormat(d3.format(".0f"))
+        .tickValues(dataTime)
+        .default(dataTime[0])
+        .on('onchange', val => {
+            d3.select('p#value-time').text((val));
+            drawGraph(songs,sliderTime.value());
+
+        });
+
+    var gTime = d3
+        .select('div#slider-time')
+        .append('svg')
+        .attr('width', 500)
+        .attr('height', 100)
+        .append('g')
+        .attr('transform', 'translate(30,30)');
+
+    gTime.call(sliderTime);
+
+    d3.select('p#value-time').text((sliderTime.value()));
+
+
+    drawGraph(songs,sliderTime.value());
+
+
+
+});
+
+function drawGraph(allData,year) {
+    var songs = [];
+    allData.forEach(d=>{
+        if(d.tracks_track_date_created == year)
+            songs.push(d);
+    });
+    console.log(songs);
+    d3.selectAll(".foreground").remove();
+    // Make yScale for each dimension
     features.forEach(d => {
         yScale[d] = d3.scaleLinear().range([contentHeight, 0])
             .domain(d3.extent(songs, function (fea) {
@@ -56,14 +138,16 @@ d3.csv("dataset/dataset.csv",function (error, songs) {
         .attr("id",d=>{return "path"+ d.track_id;})
         .attr("d", path)
         .on("mouseover",d=>{
-            console.log(d);
-            foreground.style("opacity","0.3");
+            foreground.style("opacity","0.1");
             d3.select("#path"+ d.track_id).style("stroke-width","4px").style("opacity","1");
-            titleGroup.append("text").attr("class","title").text("Title: "+ d.title);
+            titleGroup.append("text")
+                .attr("class","title")
+                .text( "Title: " + d.title);
         })
         .on("mouseout",d=>{
             // console.log("mouse out");
-            brush();
+            // brush();
+            foreground.style("opacity","0.7");
             d3.select("#path"+ d.track_id).style("stroke-width","1px").style("opacity","0.7");
             d3.selectAll(".title").remove();
         });
@@ -103,9 +187,9 @@ d3.csv("dataset/dataset.csv",function (error, songs) {
                 .extent([[-10, 0], [10, contentHeight]])
                 .on("brush", brush)
                 .on("end", brush)
-             );
+            );
         });
-});
+}
 
 function path(d) {
     // console.log('333333')
@@ -166,20 +250,20 @@ function brush() {
 
     var selected = [];
     // Update foreground to only display selected values
-    foreground.style("opacity",function(d) {
-            return actives.every(function(active) {
-                let result = active.extent[1] <= d[active.feature] && d[active.feature] <= active.extent[0];
-                if(result)selected.push(d);
-                return result;
-            }) ? "1" : "0.1";
-        });
-    // foreground.style("display", function(d) {
-    //     return actives.every(function(active) {
-    //         let result = active.extent[1] <= d[active.feature] && d[active.feature] <= active.extent[0];
-    //         if(result)selected.push(d);
-    //         return result;
-    //     }) ? null : "none";
-    // });
+    // foreground.style("opacity",function(d) {
+    //         return actives.every(function(active) {
+    //             let result = active.extent[1] <= d[active.feature] && d[active.feature] <= active.extent[0];
+    //             if(result)selected.push(d);
+    //             return result;
+    //         }) ? "1" : "0.1";
+    //     });
+    foreground.style("display", function(d) {
+        return actives.every(function(active) {
+            let result = active.extent[1] <= d[active.feature] && d[active.feature] <= active.extent[0];
+            if(result)selected.push(d);
+            return result;
+        }) ? null : "none";
+    });
     // (actives.length>0)?out.text(d3.tsvFormat(selected.slice(0,24))):out.text(d3.tsvFormat(sample_data.slice(0,24)));;
 
 }
