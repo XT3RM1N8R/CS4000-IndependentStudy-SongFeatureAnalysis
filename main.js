c213ff7578387790551d6dafdcc98268ab9082a3
Darien Sokolov <darien.sokolov@yahoo.com>
2019-04-08 06:55:52 -0500

    Edits to improve code structure and readability

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 database=[];
-data=[]
+data=[];
+const testSize = 500; // The size of our test data for development speed
+
 d3.csv("./Dataset/dataset_full(optimal).csv")
     .row(function(d) {
         data.push(d);
@@ -11,9 +13,7 @@ d3.csv("./Dataset/dataset_full(optimal).csv")
             +d.liveness,
             +d.speechiness, +d.tempo, +d.valence]); })
     .get(function(error, rows) {
-        console.log("About to print dataset");
-        //console.log(database) //we can get the data in csv file really quick
-        data_min=database.slice(0,1000) // Test with 1000 data point first.
+        const data_min = database.slice(0,testSize); // Limit the test data for quick debugging
         startWorker({dataset:data_min,
                      epsilon: 1,        // epsilon is learning rate (10 = default)
                      perplexity: 30,    // roughly how many neighbors each point influences (30 = default)
@@ -21,87 +21,102 @@ d3.csv("./Dataset/dataset_full(optimal).csv")
     });
 
 function getcluster(dataset){
-    var result=[];
-    var result2=[];
-//give number of clusters we want
+    let clusterSet = [];
+    let centroids = [];
+    
+    //give number of clusters we want
     clusters.k(10);
 
-//number of iterations (higher number gives more time to converge), defaults to 1000
+    //number of iterations (higher number gives more time to converge), defaults to 1000
     clusters.iterations(750);
 
-//data from which to identify clusters, defaults to []
+    //data from which to identify clusters, defaults to []
     clusters.data(dataset);
 
-    result=clusters.clusters();
-    result.forEach(function(d){
-        return result2.push(d.centroid)
-    })
-    return result2
-    console.log(result2)
-
+    clusterSet = clusters.clusters();
+    clusterSet.forEach(function(d){ // Save the centroids of each cluster
+        return centroids.push(d.centroid)
+    });
+    
+    console.log(centroids);
+    return centroids;
 }
 
 function draw_network(tsne_data){
-    var totalscore = [];
-    var links = [];
-    var link2 = [];
+    let totalscore = [];
+    let links = [];
+    let link2 = [];
     for (i = 0; i < tsne_data.length - 1; i++) {
-        var scorefinal = [];
-        var link1 = [];
+        let scorefinal = [];
+        let link1 = [];
         for (j = i + 1; j < tsne_data.length; j++) {
 
             //Calculate distance based on Euclidean Distance
-            scorefinal.push(euclidean(tsne_data[i], tsne_data[j]))
+            scorefinal.push(euclidean(tsne_data[i], tsne_data[j]));
             link1.push({"source": i, "target": j})
 
             //Calculate distance based on T-sne output and Euclidean Distance
             // scorefinal.push(euclideanDistance(tsne_score[i],tsne_score[j]))
         }
-        totalscore.push(scorefinal)
+        totalscore.push(scorefinal);
         link2.push(link1)
-
     }
-    data1 = d3.merge(totalscore);
+    
+    var data1 = d3.merge(totalscore);
     links = d3.merge(link2);
     links.forEach(function (d, i) {
         d.value = data1[i];
-    });
-    links.forEach(function (d, i) {
         d.i = i;
     });
 
-    var nodes=data.slice(0,1000);
-    console.log(nodes)
-    var width = 1000,
-        height = 1000;
-    var colors = colorbrewer.Spectral[9];
-    var dataset=totalscore;
-    var scale = d3.scale.linear().domain([math.min(dataset), math.max(dataset)]).range([0, 1])
-    var colorScale = d3.scale.quantize()
+    var nodes=data.slice(0,testSize);
+    console.log(nodes);
+    
+    const dataset = totalscore;
+    var scale = d3.scale.linear()
+        .domain([math.min(dataset), math.max(dataset)])
+        .range([0, 1]);
+    
+    const colors = colorbrewer.Spectral[9];
+    const colorScale = d3.scale.quantize()
         .domain([0, 1])
-        .range(colors)
+        .range(colors);
+    
+    const width = 1000,
+          height = 1000;
     var force = d3.layout.force()
         .charge(-20)
         .linkDistance(30)
         .size([width, height]);
+    
+    var svg = d3.select("#network")
+    .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+    
     var x = d3.scale.linear()
         .domain([0, math.max(totalscore)])
         .range([280, 450])
         .clamp(true);
+    
     var brush = d3.svg.brush()
         .y(x)
         .extent([0, 0]);
-    var control2 = d3.select("#controller").append("svg")
+
+    var links_g = svg
+    .append("g")
+        .attr("id", "links");
+    var nodes_g = svg
+    .append("g")
+        .attr("id", "nodes");
+    
+    var control2 = d3.select("#controller")
+    .append("svg")
         .attr("width",650)
         .attr("height",100);
-    var svg = d3.select("#network").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    var links_g = svg.append("g");
-    var nodes_g = svg.append("g");
-
-    control2.append("g")
+    
+    control2
+    .append("g")
         .attr("transform", "translate(0,50) rotate(270)")
         .attr("class", "x axis")
         .call(d3.svg.axis()
@@ -117,7 +132,9 @@ function draw_network(tsne_data){
             return this.parentNode.appendChild(this.cloneNode(true));
         })
         .attr("class", "halo");
-    var slider = control2.append("g")
+    
+    var slider = control2
+    .append("g")
         .attr("class", "slider")
         .attr("transform", "translate(0,0),rotate(270)")
         .call(brush);
@@ -125,43 +142,43 @@ function draw_network(tsne_data){
     slider.selectAll(".extent,.resize")
         .remove();
 
-    var handle = slider.append("circle")
+    var handle = slider
+    .append("circle")
         .attr("class", "handle")
         .attr("transform", "translate(" + (-50) + ",0)")
         .attr("r", 5);
+    
+    // Handle the link threshold slider
     function brushed() {
-        var value = brush.extent()[0];
+        let threshold;
         if (d3.event.sourceEvent) {
-            value = x.invert(d3.mouse(this)[1]);
-            brush.extent([value, value]);
+            threshold = x.invert(d3.mouse(this)[1]);
+            brush.extent([threshold, threshold]);
+        } else {
+            threshold = brush.extent()[0];
         }
-        handle.attr("cy", x(value));
-        var threshold = value;
-
-        var thresholded_links = links.filter(function (d) {
-
+        
+        handle.attr("cy", x(threshold));
+        
+        // Find the links that satisfy the threshold
+        const thresholded_links = links.filter(function (d) {
             return (d.value <= threshold);
         });
-        console.log(thresholded_links)
-
+        console.log(thresholded_links);
 
         force
             .links(thresholded_links);
-
-        var active_value;
+        
         var link = links_g.selectAll(".link")
             .data(thresholded_links, function (d) {
                 return d.i;
             });
-
-        link.enter().append("line")
+        link.enter()
+        .append("line")
             .attr("class", "link")
             .attr("stroke-width", function (d) {
                 return Math.sqrt(d.value);})
             .attr("stroke", function(d) { return 'lightgray' });
-
-
-
         link.exit().remove();
 
         force.on("tick", function () {
@@ -188,32 +205,32 @@ function draw_network(tsne_data){
     force
         .nodes(nodes);
 
-
     brush.on("brush", brushed);
     slider
         .call(brush.extent([0, 0]))
         .call(brush.event);
 
-
     var node = nodes_g.selectAll(".node")
         .data(nodes)
-        .enter().append("circle")
+        .enter()
+    .append("circle")
         .attr("class", "node")
         .attr("r", 3)
         .style("fill", "steelblue")
         .call(force.drag);
-    node.append("title")
+    node
+    .append("title")
         .text(function(d) { return d.genre; });
 
 }
 
+// Calculate the Euclidean Distance between two arrays of the same length
 function euclidean(a,b){
-var sum = 0
-var n
-for (n = 0; n < a.length; n++) {
-    sum += Math.pow(a[n] - b[n], 2)
-}
-return math.sqrt(sum)
+    let sum = 0;
+    for (let n = 0; n < a.length; n++) {
+        sum += Math.pow(a[n] - b[n], 2)
+    }
+    return math.sqrt(sum)
 }
 
 
