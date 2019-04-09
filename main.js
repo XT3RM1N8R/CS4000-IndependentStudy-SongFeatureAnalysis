97cad217f7ee0ff3f56a2bd9b6a6e2f5d66aabdf
Nhatmusic <42849391+Nhatmusic@users.noreply.github.com>
2019-04-08 22:26:35 -0500

    update node's title

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function CountGenres(data) {    // ***We could optimize this function further, b
     data.forEach(d => { // Build the list of genre occurrences
         genres.push(d.genre);
     });
-    
+
     var count;
     data.forEach((d) => {
         count = 0;
@@ -57,219 +57,8 @@ function CountGenres(data) {    // ***We could optimize this function further, b
 
     //sort count_genre array
     count_genre.sort( function ( a, b ) { return b.number - a.number; } );
-    
+
     return count_genre;
-}
-
-// Get a set of cluster centroids based on the given data
-function getcluster(data){
-    let clusterSet = [];
-    let centroids = [];
-    
-    //give number of clusters we want
-    clusters.k(10);
-
-    //number of iterations (higher number gives more time to converge), defaults to 1000
-    clusters.iterations(750);
-
-    //data from which to identify clusters, defaults to []
-    clusters.data(data);
-
-    clusterSet = clusters.clusters();
-    clusterSet.forEach(function(d){ // Save the centroids of each cluster
-        return centroids.push(d.centroid)
-    });
-    
-    console.log(centroids);
-    return centroids;
-}
-
-// Draw a network diagram based on the given data
-function Draw_Network(tsne_data){
-    let totalscore = [];
-    let links = [];
-    let link2 = [];
-    for (i = 0; i < tsne_data.length - 1; i++) {
-        let scorefinal = [];
-        let link1 = [];
-        for (j = i + 1; j < tsne_data.length; j++) {
-
-            //Calculate distance based on Euclidean Distance
-            scorefinal.push(Euclidean(tsne_data[i], tsne_data[j]));
-            link1.push({"source": i, "target": j})
-
-            //Calculate distance based on T-sne output and Euclidean Distance
-            // scorefinal.push(euclideanDistance(tsne_score[i],tsne_score[j]))
-        }
-        totalscore.push(scorefinal);
-        link2.push(link1)
-    }
-    
-    var data1 = d3.merge(totalscore);
-    links = d3.merge(link2);
-    links.forEach(function (d, i) {
-        d.value = data1[i];
-        d.i = i;
-    });
-    
-    var scale = d3.scale.linear()
-        .domain([math.min(totalscore), math.max(totalscore)])
-        .range([0, 1]);
-    
-    const colors = colorbrewer.Spectral[9];
-    const colorScale = d3.scale.quantize()
-                         .domain([0, 1])
-                         .range(colors);
-    
-    const width = 1000,
-          height = 1000;
-    var force = d3.layout.force()
-                    .charge(-20)
-                    .linkDistance(30)
-                    .size([width, height]);
-    
-    var svg = d3.select("#network")
-              .append("svg")
-                .attr("width", width)
-                .attr("height", height);
-    
-    var x = d3.scale.linear()
-                .domain([0, math.max(totalscore)])
-                .range([280, 450])
-                .clamp(true);
-    
-    var brush = d3.svg.brush()
-        .y(x)
-        .extent([0, 0]);
-
-    var links_g = svg
-                  .append("g")
-                    .attr("id", "links");
-    var nodes_g = svg
-                  .append("g")
-                    .attr("id", "nodes");
-    
-    var control2 = d3.select("#controller")
-                   .append("svg")
-                     .attr("width",650)
-                     .attr("height",100);
-    
-    control2
-    .append("g")
-        .attr("transform", "translate(0,50) rotate(270)")
-        .attr("class", "x axis")
-        .call(d3.svg.axis()
-            .scale(x)
-            .orient("left")
-            .tickFormat(function (d) {
-                return d;
-            })
-            .tickSize(0)
-            .tickPadding(5))
-        .select(".domain")
-        .select(function () {
-            return this.parentNode.appendChild(this.cloneNode(true));
-        })
-        .attr("class", "halo");
-    
-    var slider = control2
-                 .append("g")
-                    .attr("class", "slider")
-                    .attr("transform", "translate(0,0),rotate(270)")
-                    .call(brush);
-
-    slider.selectAll(".extent,.resize")
-        .remove();
-
-    var handle = slider
-                 .append("circle")
-                    .attr("class", "handle")
-                    .attr("transform", "translate(" + (-50) + ",0)")
-                    .attr("r", 5);
-    
-    // Handle the link threshold slider
-    function brushed() {
-        let threshold;
-        if (d3.event.sourceEvent) {
-            threshold = x.invert(d3.mouse(this)[1]);
-            brush.extent([threshold, threshold]);
-        } else {
-            threshold = brush.extent()[0];
-        }
-        
-        handle.attr("cy", x(threshold));
-        
-        // Find the links that satisfy the threshold
-        const thresholded_links = links.filter(function (d) {
-            return (d.value <= threshold);
-        });
-        //console.log(thresholded_links);
-
-        force
-            .links(thresholded_links);
-        
-        var link = links_g.selectAll(".link")
-            .data(thresholded_links, function (d) {
-                return d.i;
-            });
-        link.enter()
-        .append("line")
-            .attr("class", "link")
-            .attr("stroke-width", function (d) {
-                return Math.sqrt(d.value);})
-            .attr("stroke", function(d) { return 'lightgray' });
-        link.exit().remove();
-
-        force.on("tick", function () {
-            link.attr("x1", function (d) {
-                return d.source.x;
-            })
-                .attr("y1", function (d) {
-                    return d.source.y;
-                })
-                .attr("x2", function (d) {
-                    return d.target.x;
-                })
-                .attr("y2", function (d) {
-                    return d.target.y;
-                });
-
-            node.attr("cx", function(d) { return d.x; })
-                .attr("cy", function(d) { return d.y; });
-        });
-
-        force.start();
-    }
-
-    force
-        .nodes(data_min);
-
-    brush.on("brush", brushed);
-    slider
-        .call(brush.extent([0, 0]))
-        .call(brush.event);
-
-    var node = nodes_g.selectAll(".node")
-               .data(data_min)
-               .enter()
-    .append("circle")
-        .attr("class", "node")
-        .attr("r", 3)
-        .style("fill", "steelblue")
-        .call(force.drag);
-    node
-    .append("title")
-        .text(function(d) { return d.genre; });
-
-}
-
-// Calculate the Euclidean Distance between two arrays of the same length
-function Euclidean(a,b){
-    let sum = 0;
-    for (let n = 0; n < a.length; n++) {
-        sum += Math.pow(a[n] - b[n], 2)
-    }
-    return math.sqrt(sum)
 }
 
 const width = 800, height = 800,
@@ -295,18 +84,20 @@ const scatterplot = svg
                       .attr("transform", `translate(${margin.left}, ${margin.right})`)
                       .attr("id", "snodes");
 
+
+// Draw a scatterplot from the given t-SNE data
+function Draw_Scatterplot(tsne_data) {
+    UpdateDataTSNE(tsne_data);      // Update our data with the given t-SNE data
+    _Draw_Scatterplot(data_min);    // Draw the scatterplot with the updated data
+}
+
+
 // Update the data with the given t-SNE result
 function UpdateDataTSNE(data) {
     data.forEach(function(d, i) {
         data_min[i].x = d[0];  // Add the t-SNE x result to the dataset
         data_min[i].y = d[1];  // Add the t-SNE y result to the dataset
     });
-}
-
-// Draw a scatterplot from the given t-SNE data
-function Draw_Scatterplot(tsne_data) {
-    UpdateDataTSNE(tsne_data);      // Update our data with the given t-SNE data
-    _Draw_Scatterplot(data_min);    // Draw the scatterplot with the updated data
 }
 
 // Draw a scatterplot from the given data
@@ -344,6 +135,7 @@ function _Draw_Scatterplot(data){
                                         return "black";
                                     }
                                 })
+
                                 .on("mouseover", function(d) {
                                     /*d3.select(this)   // Doesn't work
                                     .append("circle")
@@ -354,7 +146,7 @@ function _Draw_Scatterplot(data){
                                     d3.select(this)     // Does work
                                         .attr("r", radius * 2);
                                     d3.select(this)
-                                    .append("text")
+                                    .append("title")
                                         .text(function(d) {
                                             if (topGenres20.includes(d.genre)) {
                                                 return "Top Genre: " + d.genre;
@@ -362,9 +154,9 @@ function _Draw_Scatterplot(data){
                                                 return "Other Genre: " + d.genre;
                                             }
                                         })
-                                        .attr("x", d.x)
-                                        .attr("y", d.y)
-                                        .style("font-size", "500%"); // This didn't solve the visibility issue :(
+                                //         .attr("x", d.x)
+                                //         .attr("y", d.y)
+                                //         .style("font-size", "500%"); // This didn't solve the visibility issue :(
                                 })
                                 .on("mouseout", function(d) {
                                     /*d3.select(this)   // Doesn't Work
@@ -399,5 +191,216 @@ function _Draw_Scatterplot(data){
 
 function getExtent(data, key) {
     return d3.extent(data.map(d => d[key]));
+}
+
+// Get a set of cluster centroids based on the given data
+function getcluster(data){
+    let clusterSet = [];
+    let centroids = [];
+
+    //give number of clusters we want
+    clusters.k(10);
+
+    //number of iterations (higher number gives more time to converge), defaults to 1000
+    clusters.iterations(750);
+
+    //data from which to identify clusters, defaults to []
+    clusters.data(data);
+
+    clusterSet = clusters.clusters();
+    clusterSet.forEach(function(d){ // Save the centroids of each cluster
+        return centroids.push(d.centroid)
+    });
+
+    console.log(centroids);
+    return centroids;
+}
+
+// Draw a network diagram based on the given data
+function Draw_Network(tsne_data){
+    let totalscore = [];
+    let links = [];
+    let link2 = [];
+    for (i = 0; i < tsne_data.length - 1; i++) {
+        let scorefinal = [];
+        let link1 = [];
+        for (j = i + 1; j < tsne_data.length; j++) {
+
+            //Calculate distance based on Euclidean Distance
+            scorefinal.push(Euclidean(tsne_data[i], tsne_data[j]));
+            link1.push({"source": i, "target": j})
+
+            //Calculate distance based on T-sne output and Euclidean Distance
+            // scorefinal.push(euclideanDistance(tsne_score[i],tsne_score[j]))
+        }
+        totalscore.push(scorefinal);
+        link2.push(link1)
+    }
+
+    var data1 = d3.merge(totalscore);
+    links = d3.merge(link2);
+    links.forEach(function (d, i) {
+        d.value = data1[i];
+        d.i = i;
+    });
+
+    var scale = d3.scale.linear()
+        .domain([math.min(totalscore), math.max(totalscore)])
+        .range([0, 1]);
+
+    const colors = colorbrewer.Spectral[9];
+    const colorScale = d3.scale.quantize()
+        .domain([0, 1])
+        .range(colors);
+
+    const width = 1000,
+        height = 1000;
+    var force = d3.layout.force()
+        .charge(-20)
+        .linkDistance(30)
+        .size([width, height]);
+
+    var svg = d3.select("#network")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var x = d3.scale.linear()
+        .domain([0, math.max(totalscore)])
+        .range([280, 450])
+        .clamp(true);
+
+    var brush = d3.svg.brush()
+        .y(x)
+        .extent([0, 0]);
+
+    var links_g = svg
+        .append("g")
+        .attr("id", "links");
+    var nodes_g = svg
+        .append("g")
+        .attr("id", "nodes");
+
+    var control2 = d3.select("#controller")
+        .append("svg")
+        .attr("width",650)
+        .attr("height",100);
+
+    control2
+        .append("g")
+        .attr("transform", "translate(0,50) rotate(270)")
+        .attr("class", "x axis")
+        .call(d3.svg.axis()
+            .scale(x)
+            .orient("left")
+            .tickFormat(function (d) {
+                return d;
+            })
+            .tickSize(0)
+            .tickPadding(5))
+        .select(".domain")
+        .select(function () {
+            return this.parentNode.appendChild(this.cloneNode(true));
+        })
+        .attr("class", "halo");
+
+    var slider = control2
+        .append("g")
+        .attr("class", "slider")
+        .attr("transform", "translate(0,0),rotate(270)")
+        .call(brush);
+
+    slider.selectAll(".extent,.resize")
+        .remove();
+
+    var handle = slider
+        .append("circle")
+        .attr("class", "handle")
+        .attr("transform", "translate(" + (-50) + ",0)")
+        .attr("r", 5);
+
+    // Handle the link threshold slider
+    function brushed() {
+        let threshold;
+        if (d3.event.sourceEvent) {
+            threshold = x.invert(d3.mouse(this)[1]);
+            brush.extent([threshold, threshold]);
+        } else {
+            threshold = brush.extent()[0];
+        }
+
+        handle.attr("cy", x(threshold));
+
+        // Find the links that satisfy the threshold
+        const thresholded_links = links.filter(function (d) {
+            return (d.value <= threshold);
+        });
+        //console.log(thresholded_links);
+
+        force
+            .links(thresholded_links);
+
+        var link = links_g.selectAll(".link")
+            .data(thresholded_links, function (d) {
+                return d.i;
+            });
+        link.enter()
+            .append("line")
+            .attr("class", "link")
+            .attr("stroke-width", function (d) {
+                return Math.sqrt(d.value);})
+            .attr("stroke", function(d) { return 'lightgray' });
+        link.exit().remove();
+
+        force.on("tick", function () {
+            link.attr("x1", function (d) {
+                return d.source.x;
+            })
+                .attr("y1", function (d) {
+                    return d.source.y;
+                })
+                .attr("x2", function (d) {
+                    return d.target.x;
+                })
+                .attr("y2", function (d) {
+                    return d.target.y;
+                });
+
+            node.attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
+        });
+
+        force.start();
+    }
+
+    force
+        .nodes(data_min);
+
+    brush.on("brush", brushed);
+    slider
+        .call(brush.extent([0, 0]))
+        .call(brush.event);
+
+    var node = nodes_g.selectAll(".node")
+        .data(data_min)
+        .enter()
+        .append("circle")
+        .attr("class", "node")
+        .attr("r", 3)
+        .style("fill", "steelblue")
+        .call(force.drag);
+    node
+        .append("title")
+        .text(function(d) { return d.genre; });
+
+}
+
+// Calculate the Euclidean Distance between two arrays of the same length
+function Euclidean(a,b){
+    let sum = 0;
+    for (let n = 0; n < a.length; n++) {
+        sum += Math.pow(a[n] - b[n], 2)
+    }
+    return math.sqrt(sum)
 }
 
